9485fe37fdd29fa49d46b1a36fbfdcf7626d8329
Eyeglass-tidbug <113344625+Eyeglass-tidbug@users.noreply.github.com>
2024-02-26 20:30:56 +0700

    Header New
    Plese use this header
    if there is error please contact me

---
+++ header.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="header.css">
+    </head>
+    <header>
+        <h2 class="menu-item"><a id="Logo" href="homepage.php">KHAOTOM</a></h2>
+        <div class="navigation">
+            <ul class="dropdown">
+                <a class="dropdown-nagivation" href="homepagephp.php?Gender=w">WOMEN</a>
+                <ul class="dropdown-body">
+                    <li><a href="homepagephp.php?Gender=w&Type=shirt">SHIRT</a></li>
+                    <li><a href="homepagephp.php?Gender=w&Type=pants">PANTS</a></li>
+                    <li><a href="homepagephp.php?Gender=w&Type=hoodie">HOODIE</a></li>
+                </ul>
+            </ul>
+            <ul class="dropdown">
+                <a class="dropdown-nagivation" href="homepagephp.php?Gender=m">MEN</a>
+                <ul class="dropdown-body">
+                    <li><a href="homepagephp.php?Gender=m&Type=shirt">SHIRT</a></li>
+                    <li><a href="homepagephp.php?Gender=m&Type=pants">PANTS</a></li>
+                    <li><a href="homepagephp.php?Gender=m&Type=hoodie">HOODIE</a></li>
+                </ul>
+            </ul>
+            <ul class="dropdown">
+                <a class="dropdown-nagivation" href="homepagephp.php?Gender=k">KIDS</a>
+                <ul class="dropdown-body">
+                    <li><a href="homepagephp.php?Gender=k&Type=shirt">SHIRT</a></li>
+                    <li><a href="homepagephp.php?Gender=k&Type=pants">PANTS</a></li>
+                    <li><a href="homepagephp.php?Gender=k&Type=hoodie">HOODIE</a></li>
+                </ul>
+            </ul>
+        </div>
+    </header>
+</html>
